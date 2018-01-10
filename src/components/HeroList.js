9c4cedf2fd221f9392a576b91d8994b24f2bdf13
Mohammad Mohaimen Chowdhury <mmc.dp11@gmail.com>
2018-01-10 19:17:50 +0600

    Update HeroList.js

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -7,19 +7,23 @@ class HeroList extends Component {
   render(){
     return(
       <div>
-				<h4>Your Hero Squad</h4>
-				<ul className="list-group">
-					{
-						this.props.heroes.map((item) => {
-							return (
-								<li key={item.id} className="list-group-item">
-									<div className="list-item">{item.name}</div>
-                  <div onClick={() => this.props.removeCharacterById(item.id)} className="list-item right-button">x</div>
-								</li>
-							);
-						})
-					}
-				</ul>
+	<h4>Your Hero Squad</h4>
+	<ul className="list-group">
+	{
+		this.props.heroes.map((item) => {
+			return (
+				<li key={item.id} className="list-group-item">
+					<div className="list-item">{item.name}</div>
+                  			<div 
+					onClick={() => this.props.removeCharacterById(item.id)} 
+    					className="list-item right-button">
+						x
+	  				</div>
+				</li>
+			);
+		})
+	}
+	</ul>
       </div>
     );
   }
